bc148ad1f7b98786e212ac1a495eed07b92d64fd
brwngld <bern587@yahoo.com>
2023-08-16 18:31:58 +0100

    Done

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-common_elements = __import__('3-common_elements').common_elements
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
