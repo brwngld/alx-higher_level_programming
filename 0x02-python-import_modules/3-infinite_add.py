1fd407b0a1a19aa923080baff1e31cf7fee38504
brwngld <bern587@yahoo.com>
2023-08-16 12:42:43 +0100

    Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-if _name_ == "_main_":
-    from sys import argv
-    add = 0
-    for s in argv[1:]:
-        add += int(s)
+from sys import argv
+add = 0
+for s in argv[1:]:
+    add += int(s)
 
-        print("{:d}".format(add))
+    print("{:d}".format(add))
